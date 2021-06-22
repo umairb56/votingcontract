9242d0224f29639025933d5a3d61da47ac6f71fe
umairb56 <83267668+umairb56@users.noreply.github.com>
2021-06-22 12:06:26 +0530

    Create controller.sol

---
+++ controller.sol
@@ -0,0 +1,38 @@
+contract Controller{
+    
+    storage
+    voting
+    government
+    
+    registerVoter( x = details you want for a voter inside a struct ) {
+        storage.addVoter(msg.sender, x)
+    }
+    
+    registerCandidate( x = details you want for a candidate inside a struct ) {
+        // checks
+        require();
+        // state modification
+        // current contract state modification to be done here
+        // interaction
+        // call to some other function/contract
+        storage.addCandidate(msg.sender, x);
+    }
+    
+    changeGovernmentAddress( newGov){
+        government = newGov;
+    }
+    
+    getVoterDetails()
+    getCandidateDetails();
+    
+    
+    startVoting()
+    endVoting()
+    voteForCandidate();
+    
+    getVoteingStatus();
+    getVotes();
+    getWinner();
+    getCandidates();
+    
+}
