07d9f3c62d648ed0a7f1b07e9f6d7b38ed273657
umairb56 <83267668+umairb56@users.noreply.github.com>
2021-06-22 12:07:44 +0530

    Create storage.sol

---
+++ storage.sol
@@ -0,0 +1,35 @@
+contract Storage{
+    
+    struct Voter
+    struct Candidate
+    mapping(key => voter);
+    voterCounter
+    mapping(key => Candidate);
+    candidateCounter;
+    
+    controller
+    
+    addVoter()
+    addCandidate()
+    
+    getVoter()
+    getCandidate()
+    
+    getNumberOfVoters()
+    getNumberOfCandidates()
+    
+    getAllCandidates()
+    getAllCandidatesId()
+    
+    bool isEntered;
+    
+    transfer(address, 100){
+        require(!isEntered);
+        isEntered = true;
+        require(balance[msg.sender] >= amt);
+        balance[address] -= amt;
+        address.transfer(amt);
+        isEntered = false;
+    }
+    
+}
